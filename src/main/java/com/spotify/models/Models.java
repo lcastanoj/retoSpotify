c7e40a037d64441c2ee6af96086ca9a06e6a7fd5
Lucas Castaño <laucasj13@gmail.com>
2024-07-11 11:40:40 -0500

    funcional hasta que da clic en reproducir

--- src/main/java/com/spotify/models/Models.java
+++ src/main/java/com/spotify/models/Models.java
@@ -1,12 +1,14 @@
 package com.spotify.models;
 
+import com.spotify.utils.Excel;
+
 import java.util.ArrayList;
 import java.util.Map;
 
 public class Models {
     private static String lblWelcome;
 
-    static ArrayList<Map<String, String>> data = CallData.extractTo();
+    static ArrayList<Map<String, String>> data = Excel.extractTo();
 
     public static String getLblWelcome() {
         return data.get(0).get("validate1");
